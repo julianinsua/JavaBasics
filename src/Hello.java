7c64f10668c27f7e1c40705d2c206c12478b76aa
julian <jii.julious@gmail.com>
2022-04-14 00:05:02 -0300

    fixed variables on print lines

--- src/Hello.java
+++ src/Hello.java
@@ -4,9 +4,10 @@ public class Hello {
         float minFloat = Float.MIN_VALUE, maxFloat = Float.MAX_VALUE;
         double minDouble = Double.MIN_VALUE, maxDouble = Double.MAX_VALUE;
         short minShort = Short.MIN_VALUE, maxShort = Short.MAX_VALUE;
+
         System.out.println("Integers from " + minInt + " to " + maxInt );
         System.out.println("Floats from " + minFloat + " to " + maxFloat);
-        System.out.println("Doubles from " + maxDouble + " to " + maxDouble);
-        System.out.println("Shorts from " + maxShort + " to " + maxShort);
+        System.out.println("Doubles from " + minDouble + " to " + maxDouble);
+        System.out.println("Shorts from " + minShort + " to " + maxShort);
     }
 }
