2c085f7156b94ee8307897d5f388300f2cab3904
julian <jii.julious@gmail.com>
2022-04-14 00:03:06 -0300

    reated variables and explored maximum and minimum values

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        int minInt = Integer.MIN_VALUE, maxInt = Integer.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE, maxFloat = Float.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE, maxDouble = Double.MAX_VALUE;
+        short minShort = Short.MIN_VALUE, maxShort = Short.MAX_VALUE;
+        System.out.println("Integers from " + minInt + " to " + maxInt );
+        System.out.println("Floats from " + minFloat + " to " + maxFloat);
+        System.out.println("Doubles from " + maxDouble + " to " + maxDouble);
+        System.out.println("Shorts from " + maxShort + " to " + maxShort);
     }
 }
