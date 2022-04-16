322d570ecc18d664236477d29ece5df40aa76844
julian <jii.julious@gmail.com>
2022-04-16 20:31:03 -0300

    Looping

---
+++ src/com/pepito/App.java
@@ -0,0 +1,28 @@
+package com.pepito;
+
+import java.util.Set;
+
+public class App {
+  public static void main ( String[] args ) {
+    System.out.println("EVEN TEST");
+    for (int i = 1; i < 25; i++)
+      System.out.println("is " + i + " even? " + (isEven(i) ? "yes" : "no"));
+
+    System.out.println("PRIME TEST");
+    for (int i = 1; i < 100; i++) {
+      if (isPrime(i)) System.out.println("Prime number: " + i);
+    }
+  }
+
+  public static boolean isEven ( int number ) {
+    return number % 2 == 0;
+  }
+
+  public static boolean isPrime ( int number ) {
+    if (number == 1) return true;
+    for (int i = 2; i <= ( int ) Math.sqrt(number); i++) {
+      if (number % i == 0) return false;
+    }
+    return true;
+  }
+}
